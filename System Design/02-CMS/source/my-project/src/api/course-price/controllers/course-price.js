03c7e664b1fd1710a066e4e41d7b32fbf556396c
The Brown Box <63064221+thebrownbox@users.noreply.github.com>
2023-08-18 14:59:17 +0700

    update

---
+++ System Design/02-CMS/source/my-project/src/api/course-price/controllers/course-price.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-price controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::course-price.course-price');
---
+++ System Design/02-CMS/source/my-project/src/api/course-price/services/course-price.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-price service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::course-price.course-price');
